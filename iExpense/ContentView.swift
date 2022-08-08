d9ac08be3b354265019204a42b981760e0e7aeae
Jiaming Guo <j23guo@gmail.com>
2022-08-07 21:47:32 -0400

    Initial Commit

---
+++ iExpense/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iExpense
+//
+//  Created by Jiaming Guo on 2022-08-07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Jiaming Guo on 2022-08-07.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
